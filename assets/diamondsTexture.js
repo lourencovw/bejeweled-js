b3024d590e1ce4e6853faf7fff68700ccb801d8c
Lourenco Werneck <lourencovw@gmail.com>
2023-06-22 01:59:32 -0300

    Constants

---
+++ assets/diamondsTexture.js
@@ -0,0 +1,7 @@
+export const DIAMONDS = {
+    'blue': PIXI.Texture.from('./assets/diamonds/blue.png'),
+    'green': PIXI.Texture.from('./assets/diamonds/green.png'),
+    'pink': PIXI.Texture.from('./assets/diamonds/pink.png'),
+    'red': PIXI.Texture.from('./assets/diamonds/red.png'),
+    'yellow': PIXI.Texture.from('./assets/diamonds/yellow.png'),
+}
